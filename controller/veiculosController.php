fc0f512d5bc6449f9709b52e832d0c7f442d7daf
Erickson Ferreira <erickson.rinho@hotmail.com>
2020-03-03 00:09:05 -0300

    iniciando cadastro de veiculos

---
+++ controller/veiculosController.php
@@ -0,0 +1,22 @@
+<?php
+require ("conexaoBanco.php");
+$veiculo= $_POST["veiculo"];
+$placa= $_POST["placa"];
+$modelo= $_POST["modelo"];
+$fabricante = $_POST["fabricante"];
+$cor= $_POST["cor"];
+$idUser= $_SESSION['id'];
+
+$sql2 = "INSERT INTO Veiculos(tipo,placa,modelo,fabricante,cor,id_usuario) VALUES(:tipo,:placa,:modelo,:fabricante,:cor,:user)";
+$query2 =$conn->prepare($sql2);
+$query2->bindValue(":tipo",$veiculo);
+$query2->bindValue(":placa",$placa);
+$query2->bindValue(":modelo",$modelo);
+$query2->bindValue(":fabricante",$fabricante);
+$query2->bindValue(":cor",$cor);
+$query2->bindValue(":user",$idUser);
+$query2->execute();
+
+header('location:../Views/addVeiculo.php');
+
+ ?>
