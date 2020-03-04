df2dbb0059e28e4f8403b3b2a28381a6f8174995
augustofarias <vinicinho1237@gmail.com>
2020-03-04 10:55:48 -0300

    adicionando o login do usuário

---
+++ Login_Usuario.php
@@ -0,0 +1,42 @@
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!-- All the files that are required -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="global.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+<script src="js.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+<!-- Where all the magic happens -->
+<!-- LOGIN FORM -->
+<div class="text-center" style="padding:50px 0">
+	<div class="logo">Entrar</div>
+	<!-- Main Form -->
+	<div class="login-form-1">
+		<form id="login-form" class="text-left">
+			<div class="login-form-main-message"></div>
+			<div class="main-login-form">
+				<div class="login-group">
+					<div class="form-group">
+						<label for="lg_username" class="sr-only">Nome</label>
+						<input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="Nome de usuário">
+					</div>
+					<div class="form-group">
+						<label for="lg_password" class="sr-only">Senha</label>
+						<input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="Senha">
+					</div>
+				</div>
+				<button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
+			</div>
+			<div class="etc-login-form">
+				<p><a href="Recuperar_Senha.php">Esqueceu sua senha?</a></p>
+				<p><a href="Cadastrar_Usuario.php">Não tem uma conta?</a></p>
+			</div>
+		</form>
+	</div>
+	<!-- end:Main Form -->
+</div>
