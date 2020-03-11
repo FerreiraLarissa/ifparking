8da2d6d24b217c1a8b3a34b6df9724cdfb189a5a
Joan Lucas <joanlucaslopes@gmail.com>
2020-03-11 00:05:49 -0300

    GH-10 Adicionando login à página de início

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="assets/css/global.css">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -33,11 +37,10 @@
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
-
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li class="active"><a href="#header">Início</a></li>
-          <li><a href="Login_Usuario.php">Entre</a></li>
+          <li><a href="#" id="btn">Entre</a></li>
 
           <li class="get-started"><a href="#">Cadastre-se</a></li>
         </ul>
@@ -45,189 +48,230 @@
 
     </div>
   </header><!-- End Header -->
-
   <!-- ======= Hero Section ======= -->
   <section id="hero">
 
     <div class="container">
       <div class="row d-flex align-items-center">
-      <div class=" col-lg-6 py-5 py-lg-0 order-2 order-lg-1" data-aos="fade-right">
-        <h1>Veja, Olhe e Modifique sua vaga</h1>
-        <h2>Administre sua vaga no Campus Igarassu</h2>
-        <a href="#about" class="btn-get-started scrollto">Cadastre-se</a>
+        <div class=" col-lg-6 py-5 py-lg-0 order-2 order-lg-1" data-aos="fade-right">
+          <h1>Veja, Olhe e Modifique sua vaga</h1>
+          <h2>Administre sua vaga no Campus Igarassu</h2>
+          <a href="#about" class="btn-get-started scrollto">Cadastre-se</a>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
+          <img src="assets/img/car.png" class="img-fluid" alt="">
+        </div>
       </div>
-      <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
-        <img src="assets/img/car.png" class="img-fluid" alt="">
-      </div>
-    </div>
     </div>
 
   </section><!-- End Hero -->
 
-  <main id="main">
-    
-    <!-- ======= Services Section ======= -->
-    <section id="services" class="services section-bg">
-      <div class="container">
-
-        <div class="section-title">
-          <h2 data-aos="fade-in">Serviços</h2>
-          <p data-aos="fade-in"></p>
-        </div>
-
-        <div class="row">
-          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-right">
-            <div class="card">
-              <div class="card-img">
-                <img src="assets/img/park.jpg" alt="...">
+  <div class="popup">
+    <div class="popup-content">
+    <img src="assets/img/close.png" class="close" id="close">
+    <div class="text-center" style="padding:0px 0">
+      <div class="logo">Entrar</div>
+      <!-- Main Form -->
+      <div class="login-form-1">
+        <form id="login-form" class="text-left">
+          <div class="login-form-main-message"></div>
+          <div class="main-login-form">
+            <div class="login-group">
+              <div class="form-group">
+                <label for="lg_username" class="sr-only">Nome</label>
+                <input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="Nome de usuário">
               </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Veja, Olhe e Modifique</a></h5>
-                <p class="card-text">Com nosso sistema você poderá, olhar e gerenciar sua vaga no estacionamento do Campus Igarassu. </p>
-                <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i>Leia Mais</a></div>
+              <div class="form-group">
+                <label for="lg_password" class="sr-only">Senha</label>
+                <input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="Senha">
               </div>
+            </div>
+            <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
+          </div>
+          <div class="etc-login-forms">
+            <p><a href="Recuperar_Senha.php">Esqueceu sua senha?</a></p>
+            <p><a href="Cadastrar_Usuario.php">Não tem uma conta?</a></p>
+          </div>
+        </form>
+      </div>
+      <!-- end:Main Form -->
+    </div>
+  </div>
+</div>  
+
+<main id="main">
+
+  <!-- ======= Services Section ======= -->
+  <section id="services" class="services section-bg">
+    <div class="container">
+
+      <div class="section-title">
+        <h2 data-aos="fade-in">Serviços</h2>
+        <p data-aos="fade-in"></p>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-right">
+          <div class="card">
+            <div class="card-img">
+              <img src="assets/img/park.jpg" alt="...">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Veja, Olhe e Modifique</a></h5>
+              <p class="card-text">Com nosso sistema você poderá, olhar e gerenciar sua vaga no estacionamento do Campus Igarassu. </p>
+              <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i>Leia Mais</a></div>
             </div>
           </div>
-          <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-left">
-            <div class="card">
-              <div class="card-img">
-                <img src="assets/img/ifpe.png" alt="...">
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Conheça nosso Campus</a></h5>
-                <p class="card-text">O Campus Igarassu fica localizado a margem da BR-101 em direção a João Pessoa, Centro e Ilha de Itamaracá</p>
-                <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i>Leia Mais</a></div>
-              </div>
-            </div>
-
-          
-    </section><!-- End Features Section -->
-
-
-    <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact section-bg">
-      <div class="container">
-
-        <div class="section-title">
-          <h2 data-aos="fade-in">Contato</h2>
         </div>
+        <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-left">
+          <div class="card">
+            <div class="card-img">
+              <img src="assets/img/ifpe.png" alt="...">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title"><a href="">Conheça nosso Campus</a></h5>
+              <p class="card-text">O Campus Igarassu fica localizado a margem da BR-101 em direção a João Pessoa, Centro e Ilha de Itamaracá</p>
+              <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i>Leia Mais</a></div>
+            </div>
+          </div>
 
-        <div class="row">
 
-          <div class="col-lg-6">
+        </section><!-- End Features Section -->
+
+
+        <!-- ======= Contact Section ======= -->
+        <section id="contact" class="contact section-bg">
+          <div class="container">
+
+            <div class="section-title">
+              <h2 data-aos="fade-in">Contato</h2>
+            </div>
 
             <div class="row">
-              <div class="col-md-12">
-                <div class="info-box" data-aos="fade-up">
-                  <i class="bx bx-map"></i>
-                  <h3>Endereço</h3>
-                  <p>BR 101, Km 43.5 - Saramandaia, PE</p>
+
+              <div class="col-lg-6">
+
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="info-box" data-aos="fade-up">
+                      <i class="bx bx-map"></i>
+                      <h3>Endereço</h3>
+                      <p>BR 101, Km 43.5 - Saramandaia, PE</p>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
+                      <i class="bx bx-envelope"></i>
+                      <h3>E-mail</h3>
+                      <p>direcao.geral@igarassu.ifpe.edu.br<br>heal-lab@gmail.com</p>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
+                      <i class="bx bx-phone-call"></i>
+                      <h3>Telefone</h3>
+                      <p>+55 081 3334-3510<br>+55 081 3634-3501</p>
+                    </div>
+                  </div>
                 </div>
+
               </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
-                  <i class="bx bx-envelope"></i>
-                  <h3>E-mail</h3>
-                  <p>direcao.geral@igarassu.ifpe.edu.br<br>heal-lab@gmail.com</p>
-                </div>
+
+              <div class="col-lg-6">
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-up">
+                  <div class="form-row">
+                    <div class="col-md-6 form-group">
+                      <input type="text" name="name" class="form-control" id="name" placeholder="Nome" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                      <div class="validate"></div>
+                    </div>
+                    <div class="col-md-6 form-group">
+                      <input type="email" class="form-control" name="email" id="email" placeholder="E-mail" data-rule="email" data-msg="Please enter a valid email" />
+                      <div class="validate"></div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Descrição" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                    <div class="validate"></div>
+                  </div>
+                  <div class="form-group">
+                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensagem"></textarea>
+                    <div class="validate"></div>
+                  </div>
+                  <div class="mb-3">
+                    <div class="loading">Carregando</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Sua mensagem foi enviada. Obrigado!</div>
+                  </div>
+                  <div class="text-center"><button type="submit">Enviar</button></div>
+                </form>
               </div>
-              <div class="col-md-6">
-                <div class="info-box mt-4" data-aos="fade-up" data-aos-delay="100">
-                  <i class="bx bx-phone-call"></i>
-                  <h3>Telefone</h3>
-                  <p>+55 081 3334-3510<br>+55 081 3634-3501</p>
-                </div>
-              </div>
+
             </div>
 
           </div>
+        </section><!-- End Contact Section -->
 
-          <div class="col-lg-6">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form" data-aos="fade-up">
-              <div class="form-row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Nome" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validate"></div>
-                </div>
-                <div class="col-md-6 form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="E-mail" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validate"></div>
-                </div>
+      </main><!-- End #main -->
+
+      <!-- ======= Footer ======= -->
+      <footer id="footer">
+
+        <div class="footer-top">
+
+          <div class="container" data-aos="fade-up">
+
+            <div class="row  justify-content-center">
+              <div class="col-lg-6">
+                <h3>IFPARKING</h3>
+                <p>Veja, Olhe e Modifique sua vaga</p>
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Descrição" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validate"></div>
+            </div>
+
+            <div class="row footer-newsletter justify-content-center">
+              <div class="col-lg-6">
+                <form action="" method="post">
+                  <input type="email" name="email" placeholder="E-mail"><input type="submit" value="Inscreva-se">
+                </form>
               </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensagem"></textarea>
-                <div class="validate"></div>
+            </div>
+
+            <div class="container footer-bottom clearfix">
+              <div class="copyright">
+                &copy; Copyright <strong><span>Heal Lab</span></strong>. Todos os direitos reservados
               </div>
-              <div class="mb-3">
-                <div class="loading">Carregando</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Sua mensagem foi enviada. Obrigado!</div>
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bocor-bootstrap-template-nice-animation/ -->
+                Designed by <a href="#">Heal Lab</a>
               </div>
-              <div class="text-center"><button type="submit">Enviar</button></div>
-            </form>
-          </div>
+            </div>
+          </footer><!-- End Footer -->
 
-        </div>
+          <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
 
-      </div>
-    </section><!-- End Contact Section -->
+          <!-- Vendor JS Files -->
+          <script src="assets/vendor/jquery/jquery.min.js"></script>
+          <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+          <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+          <script src="assets/vendor/php-email-form/validate.js"></script>
+          <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+          <script src="assets/vendor/venobox/venobox.min.js"></script>
+          <script src="assets/vendor/aos/aos.js"></script>
+          <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
 
-  </main><!-- End #main -->
+          <script>
+            document.getElementById("btn").addEventListener("click", function(){
+             document.querySelector(".popup").style.display = "flex";});
+            document.getElementById("close").addEventListener("click", function(){
+             document.querySelector(".popup").style.display ="none";});
+          </script>
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
+          <!-- Template Main JS File -->
+          <script src="assets/js/main.js"></script>
 
-    <div class="footer-top">
+        </body>
 
-      <div class="container" data-aos="fade-up">
-
-        <div class="row  justify-content-center">
-          <div class="col-lg-6">
-            <h3>IFPARKING</h3>
-            <p>Veja, Olhe e Modifique sua vaga</p>
-          </div>
-        </div>
-
-        <div class="row footer-newsletter justify-content-center">
-          <div class="col-lg-6">
-            <form action="" method="post">
-              <input type="email" name="email" placeholder="E-mail"><input type="submit" value="Inscreva-se">
-            </form>
-          </div>
-        </div>
-
-    <div class="container footer-bottom clearfix">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Heal Lab</span></strong>. Todos os direitos reservados
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bocor-bootstrap-template-nice-animation/ -->
-        Designed by <a href="#">Heal Lab</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+        </html>
